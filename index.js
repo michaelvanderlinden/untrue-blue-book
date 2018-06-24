62cf6e76a7c6262b0e6467eba2997c6732030df9
Michael Van der Linden <michael.vanderlinden@yale.edu>
2018-06-24 16:31:57 -0400

    fixed javascript bugs

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 
-
-var bush = { "AFAM": 
-				{"name": 
-					{"\n": 
-						{"Significance": 3, "The": 3}
-					}, 
-				"desc" : 
-					{"\n": 
-						{"Dub": 1, "Race,": 1, "Caribbean": 2},
-					"The": 
-						{"Dub": 1, "Race,": 1, "Caribbean": 2}
-					}
-				}
-			}
-
 var connectors = ['in', 'and', 'of', 'with', 'for'];
 
 // Returns a random integer between min (inclusive) and max (inclusive) 
@@ -64,14 +49,14 @@ function generate(dept, cat, target_len=1000) {
 }
 
 function generate_name() {
-	var words = generate(dept='AFAM', cat='name', target_len=randomInt(3, 11));
+	var words = generate(dept='ENGL', cat='name', target_len=randomInt(3, 11));
 	// for (var i = 0; i < words.length; i++)
 	// 	console.log(words[i]);
 	return words.join(' ');
 }
 
 function generate_desc() {
-	var words = generate(dept='AFAM', cat='desc', target_len=randomInt(45, 100));
+	var words = generate(dept='ENGL', cat='desc', target_len=randomInt(45, 100));
 	return words.join(' ');
 }
 
@@ -81,4 +66,4 @@ function update_all() {
 }
 
 
-document.getElementById('gen_name_desc').onclick = update_all();
+document.getElementById('gen_name_desc').onclick = update_all;
